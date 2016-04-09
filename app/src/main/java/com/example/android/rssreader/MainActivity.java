114615618a871c6f43b4160fd64dac82025b2d7e
Michael Driscoll <driscoll42@gmail.com>
2016-04-09 11:50:10 -0500

    Added Basic Saving functionality

--- app/src/main/java/com/example/android/rssreader/MainActivity.java
+++ app/src/main/java/com/example/android/rssreader/MainActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -32,6 +33,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button btnXBone;
     private Button btnAddLink;
     private EditText addedLink;
+    private EditText addedCategory;
     private ListView listApps;
     public static final String MyPREFERENCES = "MyPrefs";
     SharedPreferences sharedpreferences;
@@ -57,16 +59,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
         addedLink = (EditText) findViewById(R.id.addedLink);
+        addedCategory = (EditText) findViewById(R.id.addedCategory);
 
 
         Button btnAddLink = (Button) findViewById(R.id.btnAddLink);
         btnAddLink.setOnClickListener(this);
 
-        Button btnPS4 = (Button) findViewById(R.id.btnPS4);
+        Button btnPS4 = (Button) findViewById(R.id.btnDispCat);
         btnPS4.setOnClickListener(this);
 
-        Button btnXBone = (Button) findViewById(R.id.btnXBone);
+        Button btnXBone = (Button) findViewById(R.id.btnSaved);
         btnXBone.setOnClickListener(this);
+
+
+
 
     }
 
@@ -103,24 +109,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             listApps.setAdapter(arrayAdapterLoadedData);
 
         }
+
+        listApps.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                    long id) {
+                FeedData app = (FeedData) parent.getItemAtPosition(position);
+                realm.beginTransaction();
+                FeedData toSave = realm.where(FeedData.class).equalTo("title",app.getTitle()).findFirst();
+                toSave.setSaved(true);
+                realm.commitTransaction();
+
+            }
+        });
+
     }
 
 
     public void onClick(View v) {
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-
+        Realm realm = Realm.getInstance(realmConfig);
         switch (v.getId()) {
             //TODO: Fix the odd timing issue requiring two presses to get the data
-            case R.id.btnPS4:
+            //"http://feeds.ign.com/IGNXboxOneAll?format=xml"
+            //http://feeds.ign.com/IGNPS4All?format=xml
+            //http://feeds.ign.com/ign/wii-u-all?format=xml
+            case R.id.btnDispCat:
                 new DownloadData().execute("http://feeds.ign.com/IGNPS4All?format=xml");
-                ArrayList<FeedData> myList = new ArrayList<FeedData>();
-                ParseFeedData parsePS4 = new ParseFeedData(mFileContents, "http://feeds.ign.com/IGNPS4All?format=xml", "PS4");
-                parsePS4.process();
+                ArrayList<FeedData> catList = new ArrayList<FeedData>();
+                ParseFeedData parseCat = new ParseFeedData(mFileContents, "http://feeds.ign.com/IGNPS4All?format=xml", "PS4");
+                parseCat.process();
 
-                Realm realm = Realm.getInstance(realmConfig);
-                RealmResults<FeedData> PS4Data = realm.distinct(FeedData.class, "title");
+                RealmResults<FeedData> catData = realm.distinct(FeedData.class, "title");
 
-                for (FeedData data : PS4Data) {
+                for (FeedData data : catData) {
                     FeedData feedItems = realm.where(FeedData.class).equalTo(FeedData.TITLE, data.getTitle()).findFirst();
                     if (data.getTitle() != null) {
                         FeedData temp = new FeedData(feedItems.getTitle(),
@@ -130,44 +152,91 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                 feedItems.getFeedLink(),
                                 feedItems.getCategory(),
                                 feedItems.isSaved());
-                        myList.add(temp);
+                        catList.add(temp);
                     }
                 }
 
-                ArrayAdapter<FeedData> arrayAdapter = new ArrayAdapter<FeedData>(
-                        MainActivity.this, R.layout.list_item, myList);
-                listApps.setAdapter(arrayAdapter);
+                ArrayAdapter<FeedData> arrayAdapterCategory = new ArrayAdapter<FeedData>(
+                        MainActivity.this, R.layout.list_item, catList);
+                listApps.setAdapter(arrayAdapterCategory);
                 break;
 
-            case R.id.btnXBone:
+            case R.id.btnDispData:
+                Log.d("TestSaved", "1");
 
-                new DownloadData().execute("http://feeds.ign.com/IGNXboxOneAll?format=xml");
-                ParseFeedData parseXBone = new ParseFeedData(mFileContents);
-                parseXBone.process();
-                ArrayAdapter<FeedData> arrayAdapterXbone = new ArrayAdapter<FeedData>(
-                        MainActivity.this, R.layout.list_item, parseXBone.getFeedDatas());
-                listApps.setAdapter(arrayAdapterXbone);
-                realm = Realm.getInstance(realmConfig);
-                realm.beginTransaction();
-                realm.clear(FeedData.class);
-                realm.commitTransaction();
+                new DownloadData().execute("http://feeds.ign.com/IGNPS4All?format=xml");
+                ArrayList<FeedData> dataList = new ArrayList<FeedData>();
+                ParseFeedData parseLink = new ParseFeedData(mFileContents, "http://feeds.ign.com/IGNPS4All?format=xml", "PS4");
+                parseLink.process();
+                Log.d("TestSaved", "1");
+
+                RealmResults<FeedData> linkData = realm.distinct(FeedData.class, "title");
+
+                for (FeedData data : linkData) {
+                    FeedData feedItems = realm.where(FeedData.class).equalTo(FeedData.TITLE, data.getTitle()).findFirst();
+                    if (data.getTitle() != null) {
+                        FeedData temp = new FeedData(feedItems.getTitle(),
+                                feedItems.getLink(),
+                                feedItems.getDescription(),
+                                feedItems.getPubDate(),
+                                feedItems.getFeedLink(),
+                                feedItems.getCategory(),
+                                feedItems.isSaved());
+                        dataList.add(temp);
+                    }
+                }
+
+                ArrayAdapter<FeedData> arrayAdapterLink = new ArrayAdapter<FeedData>(
+                        MainActivity.this, R.layout.list_item, dataList);
+                listApps.setAdapter(arrayAdapterLink);
+                break;
+
+
+            case R.id.btnSaved:
+
+                ArrayList<FeedData> savedList = new ArrayList<FeedData>();
+                RealmResults<FeedData> savedData = realm.where(FeedData.class).equalTo(FeedData.SAVED,true).findAll();
+                Log.d("testsaved","Button Pressed");
+
+                for (FeedData data : savedData) {
+                    if (data.getTitle() != null) {
+                        Log.d("testsaved",data.getTitle());
+                        FeedData temp = new FeedData(data.getTitle(),
+                                data.getLink(),
+                                data.getDescription(),
+                                data.getPubDate(),
+                                data.getFeedLink(),
+                                data.getCategory(),
+                                data.isSaved());
+                        savedList.add(temp);
+                    }
+                }
+
+                ArrayAdapter<FeedData> arrayAdapterSaved = new ArrayAdapter<FeedData>(
+                        MainActivity.this, R.layout.list_item, savedList);
+                listApps.setAdapter(arrayAdapterSaved);
                 break;
 
             case R.id.btnAddLink:
+                //"http://feeds.ign.com/IGNXboxOneAll?format=xml
+                //http://feeds.ign.com/IGNPS4All?format=xml
                 //http://feeds.ign.com/ign/wii-u-all?format=xml
+
+                String newLink = addedLink.getText().toString();
+                String assignCat = addedCategory.getText().toString();
+
                 new DownloadData().execute(addedLink.getText().toString());
                 if (mFileContents == null) {
                     Toast.makeText(getApplicationContext(), "Invalid URL", Toast.LENGTH_SHORT).show();
                     AlertDialog dialog = builder.create();
-                    // Display the alert dialog on interface
                     dialog.show();
+                } else {
+                    ParseFeedData parseAddedLink = new ParseFeedData(mFileContents, newLink, assignCat);
+                    parseAddedLink.process();
+                    ArrayAdapter<FeedData> arrayAdapterAddLink = new ArrayAdapter<FeedData>(
+                            MainActivity.this, R.layout.list_item, parseAddedLink.getFeedDatas());
+                    listApps.setAdapter(arrayAdapterAddLink);
                 }
-
-                ParseFeedData parseAddedLink = new ParseFeedData(mFileContents);
-                parseAddedLink.process();
-                ArrayAdapter<FeedData> arrayAdapterAddLink = new ArrayAdapter<FeedData>(
-                        MainActivity.this, R.layout.list_item, parseAddedLink.getFeedDatas());
-                listApps.setAdapter(arrayAdapterAddLink);
                 break;
 
             default:
@@ -243,7 +312,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             URL url = new URL(urlPath);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             int response = connection.getResponseCode();
-            Log.d("DownloadData", "The response code was " + response);
+           // Log.d("DownloadData", "The response code was " + response);
             InputStream is = connection.getInputStream();
             InputStreamReader isr = new InputStreamReader(is);
 
